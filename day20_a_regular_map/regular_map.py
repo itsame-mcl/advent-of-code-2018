ff56f908b9ce0054d9180a09df2c02b000b34d32
Maxence Lagalle <maxence.lagalle@insee.fr>
2018-12-20 11:32:35 +0100

    Day 20 - Part 1 WIP : able to parse, have to compute answer

--- day20_a_regular_map/regular_map.py
+++ day20_a_regular_map/regular_map.py
@@ -1,34 +1,56 @@
 import os
-import sre_yield
+
 regex = ""
-
-# with open(os.path.join(os.path.dirname(__file__), 'input.txt'), "r", encoding="utf8") as f:
-#    for line in f:
-#        regex = line.replace("\n","").replace("^","").replace("$","")
-
-regex = "ESSWWN(E|NNENN(EESS(WNSE|)SSS|WWWSSSSE(SW|NNNE)))"
-routes = sre_yield.AllMatches(regex)
+with open(os.path.join(os.path.dirname(__file__), 'input.txt'), "r", encoding="utf8") as f:
+   for line in f:
+        regex = line.replace("\n","").replace("^","").replace("$","")
 
 rooms = set()
 doors = set()
-rooms.add((0,0))
-for route in routes:
-    walker = (0,0)
-    for char in route:
-        if char == "N":
+
+start_position = (0,0)
+walkers = [start_position]
+stack = list()
+sub_start, sub_end = [start_position], set()
+
+rooms.add(start_position)
+for char in regex:
+    if char == "N":
+        for i, walker in enumerate(walkers):
             go_to = (walker[0], walker[1] + 1)
             doors.add((walker, go_to))
-            walker = go_to
-        elif char == "S":
+            rooms.add(go_to)
+            walkers[i] = go_to
+    elif char == "S":
+        for i, walker in enumerate(walkers):
             go_to = (walker[0], walker[1] - 1)
             doors.add((go_to, walker))
-            walker = go_to
-        elif char == "E":
+            rooms.add(go_to)
+            walkers[i] = go_to
+    elif char == "E":
+        for i, walker in enumerate(walkers):
             go_to = (walker[0] + 1, walker[1])
             doors.add((walker, go_to))
-            walker = go_to
-        elif char == "W":
+            rooms.add(go_to)
+            walkers[i] = go_to
+    elif char == "W":
+        for i, walker in enumerate(walkers):
             go_to = (walker[0] - 1, walker[1])
             doors.add((go_to, walker))
-            walker = go_to
-        rooms.add(walker)
+            rooms.add(go_to)
+            walkers[i] = go_to
+    elif char == "(":
+        stack.append((sub_start, sub_end))
+        sub_start, sub_end = walkers.copy(), set()
+    elif char == "|":
+        for walker in walkers:
+            sub_end.add(walker)
+        walkers = sub_start.copy()
+    elif char == ")":
+        for walker in walkers:
+            sub_end.add(walker)
+        walkers = list(sub_end).copy()
+        sub_start, sub_end = stack.pop()
+        
+
+print(rooms)
